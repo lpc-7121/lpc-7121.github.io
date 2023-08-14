46dbd07ad11a239b21fab7b067563299f73d1818
Theokkoer1357 <49214970+Theokkoer1357@users.noreply.github.com>
2023-08-13 21:27:45 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,27 @@
 var ScoldCounter = 0;
 
-document.addEventListener("DOMContentLoaded", UpdateScoldCounter);
+const fetchParameter = async () => {
+  try {
+    const response = await fetch('https://hysp3zzhfkshrh62elol3vp6rq0zytev.lambda-url.us-east-1.on.aws/'); // No "await" here
+    const data = await response.json(); // "response" is a promise, not a Response object yet
+    console.log('Parameter value:', data.value);
+    return await data.value
+  } catch (error) {
+    console.error('Error fetching parameter:', error);
+  }
+};
+const incrementParameter = async () => {
+  try {
+    const response = await fetch('https://gvqqbvgdysgtpqghjs35befc340hhymg.lambda-url.us-east-1.on.aws/ '); // No "await" here
+    const data = await response.json(); // "response" is a promise, not a Response object yet
+    console.log('Parameter value:', data.value);
+    return await data.value
+  } catch (error) {
+    console.error('Error fetching parameter:', error);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", OnScreenLoaded);
 document.getElementById("Scold").onclick = OnScolded
 function OnScolded(){
     ScoldCounter += 1;
